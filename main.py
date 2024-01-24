38494c1ed5f18af6472298038ac969c76c9bc6b1
IDGS-803-21000035 <21000035@alumnos.utleon.edu.mx>
2024-01-23 18:39:05 -0600

    23/01/2024

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from  flask import Flask, render_template
+from  flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -42,6 +42,39 @@ def func1(n1, n2):
 @app.route("/default/<string:ab>")
 def func2(ab = "UTL"):
     return "el valor es" + ab
+
+
+#formulario
+@app.route("/multiplicar", methods = ["GET","POST"])
+def mult():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        return "<h1> La multiplicación es: {} </h1>".format(str(int(num1)*int(num2)))
+    else:
+
+        return '''
+            <form action = "/multiplicar" method = "POST">
+                <label> N1: </label>
+                <input type="text" name="n1"/><br>
+                <label> N2: </label>
+                <input type="text" name="n2"/><br>
+                <input type="submit"/>
+            </form>
+
+            '''
+#decorador para formulario1
+@app.route("/formulario1")
+def formulario():
+    return render_template("formulario1.html")
+
+@app.route("/resultado", methods = ["GET","POST"])
+def resultado():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        return "<h1> La multiplicación es: {} </h1>".format(str(int(num1)*int(num2)))
+    
 
 if __name__== "__main__":
     app.run(debug=True)
